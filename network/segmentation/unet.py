59adab338c84863fbe48cb6d2c1e5588bd78c04b
Lukas Klinger <lukas.klinger9@kit.edu>
2022-08-10 10:21:20 +0200

    added unet

--- network/segmentation/unet.py
+++ network/segmentation/unet.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 
 
@@ -55,23 +57,51 @@ class UpSamplingBlock(torch.nn.Module):
 
 
 class UNet(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels: int, channels_per_depth: List[int],  final_out_channels: int):
         super(UNet, self).__init__()
 
-        # TODO
-        self.down1 = DownSamplingBlock(in_channels, 8, use_pooling=False)
-        self.down2 = DownSamplingBlock(8, 16, use_pooling=True)
+        self.downsampling_layers = list()
+        self.downsampling_layers.append(DownSamplingBlock(in_channels, channels_per_depth[0]))
+        for current_channels_in, current_channels_out in zip(channels_per_depth[:-2], channels_per_depth[1:-1]):
+            self.downsampling_layers.append(DownSamplingBlock(current_channels_in, current_channels_out))
+        self.downsampling_layers = torch.nn.Sequential(*self.downsampling_layers)
 
-        self.up1 = UpSamplingBlock(16, 16, 8, use_transpose_conv=True)
-        self.up2 = UpSamplingBlock(8, 8, 1, use_transpose_conv=False)
+        self.intermediate_layers = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=channels_per_depth[-2], out_channels=channels_per_depth[-1], kernel_size=3,
+                            padding=1, stride=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(in_channels=channels_per_depth[-1], out_channels=channels_per_depth[-1], kernel_size=3,
+                            padding=1, stride=1),
+            torch.nn.ReLU()
+        )
+
+        channels_per_depth.reverse()
+
+        self.upsampling_layers = list()
+        for current_channels_in, current_channels_out in zip(channels_per_depth[0:-1], channels_per_depth[1:]):
+            self.upsampling_layers.append(UpSamplingBlock(current_channels_in, current_channels_out, current_channels_out))
+        self.upsampling_layers = torch.nn.Sequential(*self.upsampling_layers)
+
+        self.final_layer = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=channels_per_depth[-1], out_channels=final_out_channels, kernel_size=1),
+            torch.nn.Sigmoid()
+        )
 
     def forward(self, x):
-        # TODO
-        x_1, x_1_down = self.down1(x)
-        x_2, x_2_down = self.down2(x_1_down)
-        x_3 = self.up1(x_2_down, x_2)
-        x_4 = self.up2(x_3, x_1)
-        return x_4
+        skip_outputs = list()
+
+        for layer in self.downsampling_layers:
+            x_skip, x = layer.forward(x)
+            skip_outputs.append(x_skip)
+
+        x = self.intermediate_layers.forward(x)
+
+        skip_outputs.reverse()
+
+        for layer, skip_input in zip(self.upsampling_layers, skip_outputs):
+            x = layer.forward(x, skip_input)
+
+        return self.final_layer.forward(x)
 
 
 if __name__ == '__main__':
@@ -80,9 +110,10 @@ if __name__ == '__main__':
     from torchvision.transforms import ToTensor
 
     img = Image.open(
-        Path("~\\Documents\\data\\aml\\img_facemask_downsampled\\00000_Mask.png").expanduser()
+        Path("~\\Documents\\data\\aml\\masked128png\\00000_Mask.png").expanduser()
     )
 
-    net = UNet(3, 1)
+    net = UNet(3, [16, 32, 64], 1)
+    print(net)
     net.forward(ToTensor()(img)[None, :])
 
