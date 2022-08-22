24827d9be985d61e6ce9e2f8f9e2b11bfd2d2439
rr3072 <johannes.maul@gmx.de>
2022-08-22 12:32:52 +0200

    Create main_helper.py

---
+++ main_helper.py
@@ -0,0 +1,7 @@
+from train.general_training import train
+from utils.config_parser import ConfigParser
+import numpy as np
+from train.diffusion_model.alternative.helper import main
+
+def main():
+    main()
