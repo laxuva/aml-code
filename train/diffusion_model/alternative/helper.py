e2a30271f12aa7008896ecb0ac0b286f7885ea9e
rr3072 <johannes.maul@gmx.de>
2022-08-22 13:06:32 +0200

    Update helper.py

--- train/diffusion_model/alternative/helper.py
+++ train/diffusion_model/alternative/helper.py
@@ -168,7 +168,7 @@ class SimpleUnet(nn.Module):
         image_channels = 3
         down_channels = (64, 128, 256, 512, 1024)
         up_channels = (1024, 512, 256, 128, 64)
-        out_dim = 1
+        out_dim = 0.9
         time_emb_dim = 32
 
         # Time embedding
