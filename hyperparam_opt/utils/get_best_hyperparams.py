fea54e10386bd98021031736726dc4d42ba91f78
Lukas Klinger <lukas.klinger9@kit.edu>
2022-08-29 10:48:23 +0200

    added small script to read the best hyperparameters

---
+++ hyperparam_opt/utils/get_best_hyperparams.py
@@ -0,0 +1,16 @@
+import json
+
+import numpy as np
+
+
+def get_best_hyperparams(intermediate_results: str):
+    with open(intermediate_results, "r") as f:
+        results = json.load(f)
+
+        idx = np.argmin([r["loss"] for r in results])
+
+        return results[idx]
+
+
+if __name__ == '__main__':
+    print(get_best_hyperparams("../results/intermediate_results.json"))
