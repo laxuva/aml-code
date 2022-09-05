18d04a3e8c0d8445467fdf9d1322c531d3082fdc
rr3072 <johannes.maul@gmx.de>
2022-09-05 09:39:13 +0200

    segmentation update for new config and to export images

--- predictions/segmentation.py
+++ predictions/segmentation.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 import torch
 from PIL import Image
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, ToPILImage
 
 from metrics.segmentation.iou import iou
 from network.unet import UNet
@@ -12,16 +12,17 @@ from utils.config_parser import ConfigParser
 
 
 @torch.no_grad()
-def test_prediction(model_path, image_path, label_path, th=0.25, config_file="../configs/segmentation.yaml"):
+def test_prediction(model_path, image_path, label_path, out_path, th=0.25, config_file="../configs/segmentation.yaml"):
     config = ConfigParser.read(config_file)
     image_path = Path(image_path).expanduser()
     label_path = Path(label_path).expanduser()
+    out_path = Path(out_path).expanduser()
 
     to_tensor = ToTensor()
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    model = UNet(**config["model"])
+    model = UNet(**config["model"]["params"])
     model.to(device)
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.eval()
@@ -30,6 +31,7 @@ def test_prediction(model_path, image_path, label_path, th=0.25, config_file="..
     y = to_tensor(Image.open(label_path))
     y_pred = model.forward(x[None, :])[0].cpu().detach()
 
+    ToPILImage()(torch.tensor(y_pred > th, dtype=float)).save(out_path.joinpath(f"predicted_segmentation.png"))
     print(f"y_pred min: {y_pred.min().item()}; max: {y_pred.max().item()}")
     print(f"IOU: {iou(y_pred, y, th=th).item()}")
 
@@ -46,10 +48,13 @@ def test_prediction(model_path, image_path, label_path, th=0.25, config_file="..
     overlay.plot_y_pred_and_y_true_overlay()
 
 
+
+
 if __name__ == '__main__':
     test_prediction(
-        model_path="../train/final_model.pt",
-        image_path="~/Documents/data/aml/masked128png/45844_Mask.png",
-        label_path="~/Documents/data/aml/seg_mask128png/45844_seg.png",
+        model_path="../evaluation/segmentation/final_model.pt",
+        image_path="~/Documents/data/aml/masked128png/65959.png",
+        label_path="~/Documents/data/aml/seg_mask128png/65959.png",
+        out_path="~/Documents/data/aml/out/",
         th=0.002
     )
