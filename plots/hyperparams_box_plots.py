12f69cf917c8981fb17d6ae4ae77c27fbc199dc7
Lukas Klinger <lukas.klinger9@kit.edu>
2022-09-08 21:44:13 +0200

    added ylabel to boxplots

--- plots/hyperparams_box_plots.py
+++ plots/hyperparams_box_plots.py
@@ -47,13 +47,14 @@ def plot_box_plots(hyperparams_intermediate_results: List[Dict[str, Any]], model
         )
         if hyperparam == "channels_per_depth":
             plt.xticks(rotation=-7)
+        plt.ylabel("Best Loss")
         plt.savefig(f"box_plots/{model_name}_{hyperparam}", bbox_inches="tight")
         plt.title(hyperparam)
         plt.show()
 
 
 if __name__ == '__main__':
-    # with open("../evaluation/autoencoder/intermediate_results.json") as f:
-    #     plot_box_plots(json.load(f), "autoencoder")
+    with open("../evaluation/autoencoder/intermediate_results.json") as f:
+        plot_box_plots(json.load(f), "autoencoder")
     with open("../evaluation/diffusion_model/intermediate_results.json") as f:
         plot_box_plots(json.load(f), "diffusion_model")
